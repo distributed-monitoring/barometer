d7fd36e98e18a79b5fe26d9a93cfa53ad0127319
Toshiaki Takahashi <takahashi.tsc@ncos.nec.co.jp>
2018-08-22 05:11:05 +0000

    Separate localagent test

--- baro_tests/collectd.py
+++ baro_tests/collectd.py
@@ -21,9 +21,9 @@ import os
 import sys
 import time
 import logging
-import pika
 import config_server
 import tests
+import local_agent
 from distutils import version
 from opnfv.deployment import factory
 
@@ -240,81 +240,6 @@ class CSVClient(object):
         return metrics
 
 
-class LocalAgentClient(object):
-    """Client to request LocalAgent"""
-    def __init__(self, host, port, user, passwd):
-        """
-        Keyword arguments:
-        host -- Host URL
-        port -- Host Port
-        user -- Username
-        passwd -- Password
-        """
-        self._host = host
-        self._port = port
-        self._user = user
-        self._passwd = passwd
-
-    def set(self, file):
-        logger.error('Do nothing to LocalAgent')
-
-    def __str__(self):
-        return ('host: {0}, port: {1}, user: {2}, pass: {3}'
-                .format(self._host, self._port,
-                        self._user, (self._passwd and '<Filterd>')))
-
-
-class RestLocalAgentClient(LocalAgentClient):
-    """Client to request LocalAgent using REST"""
-    def __init__(self, host, port, user, passwd):
-        super(self.__class__, self).__init__(host, port, user, passwd)
-
-    def set(self, file):
-        logger.debug('Send to localagent using REST -- {}'.format(str(self)))
-
-        if not os.path.isfile(file):
-            print '{} is not found'.format(file)
-            return False
-        filename = os.path.basename(file)
-
-        url = 'http://{0}:{1}/collectd/conf'.format(self._host, self._port)
-        config = {'file': (filename, open(file, 'r'))}
-        requests.post(url, files=config)
-
-        return True
-
-
-class PubLocalAgentClient(LocalAgentClient):
-    """Client to request LocalAgent using AMQP Publish"""
-    def __init__(self, host, port, user, passwd):
-        super(self.__class__, self).__init__(host, port, user, passwd)
-
-    def set(self, file):
-        logger.debug('Send to localagent using AMQP Publish -- {}'
-                     .format(str(self)))
-
-        if not os.path.isfile(file):
-            print '{} is not found'.format(file)
-            return False
-        filename = os.path.basename(file)
-        filebody = open(file, 'r').read()
-        message = filename + '/' + filebody
-
-        credentials = pika.PlainCredentials(self._user, self._passwd)
-        connection = pika.BlockingConnection(pika.ConnectionParameters(
-                host=self._host, port=int(self._port),
-                credentials=credentials))
-        channel = connection.channel()
-        channel.exchange_declare(exchange='collectd-conf',
-                                 exchange_type='fanout')
-        channel.basic_publish(exchange='collectd-conf',
-                              routing_key='',
-                              body=message)
-
-        connection.close()
-        return True
-
-
 def get_csv_categories_for_ipmi(conf, compute_node):
     """Get CSV metrics.
 
@@ -349,26 +274,6 @@ def _process_result(compute_node, out_plugin, test, result, results_list, node):
             'Test case for {0} with {1} FAILED on {2}.'.format(
                 node, out_plugin, test))
     results_list.append((compute_node, out_plugin, test, result))
-
-
-def _process_localagent_result(compute_node, testfunc,
-                               result, results_list, node):
-    """Print LocalAgent test result and append it to results list.
-
-    Keyword arguments:
-    testfunc -- localagent function name
-    result -- boolean test result
-    results_list -- results list
-    """
-    if result:
-        logger.info(
-            'Test case for {0} with LocalAgent PASSED on {1}.'.format(
-                node, testfunc))
-    else:
-        logger.error(
-            'Test case for {0} with LocalAgent FAILED on {1}.'.format(
-                node, testfunc))
-    results_list.append((compute_node, "LocalAgent", testfunc, result))
 
 
 def _print_label(label):
@@ -446,42 +351,8 @@ def _print_final_result_of_plugin(
     return print_line
 
 
-def _print_result_of_localagent(compute_ids, results):
-    """Print results of LocalAgent.
-
-    Keyword arguments:
-    compute_ids -- list of compute node IDs
-    results -- results list
-    """
-    testname = "LocalAgent"
-    print_line = ''
-    for id in compute_ids:
-        all_result = \
-            'FAIL' if [
-                testfunc for comp_id, testname, testfunc, res in results
-                if comp_id == id and not res] else 'PASS'
-        print_line += '| ' + all_result + '   '
-    logger.info(
-        '| {}'.format(testname) + (' ' * (15 - len(testname)))
-        + print_line + '|')
-
-    for testfunc in ['Server', 'InfoFetch']:
-        print_line = ''
-        for id in compute_ids:
-            if (id, testname, testfunc, True) in results:
-                print_line += ' PASS   |'
-            elif (id, testname, testfunc, False) in results:
-                print_line += ' FAIL   |'
-            else:
-                print_line += ' SKIP   |'
-        logger.info(
-            '|  {}'.format(testfunc) + (' ' * (14-len(testfunc)))
-            + '|' + print_line)
-
-
 def print_overall_summary(
-        compute_ids, tested_plugins, aodh_plugins, results, out_plugins,
-        agent_results):
+        compute_ids, tested_plugins, aodh_plugins, results, out_plugins):
     """Print overall summary table.
 
     Keyword arguments:
@@ -489,7 +360,6 @@ def print_overall_summary(
     tested_plugins -- list of plug-ins
     results -- results list
     out_plugins --  list of used out plug-ins
-    agent_results -- localagent results list
     """
     compute_node_names = ['Node-{}'.format(i) for i in range(
         len((compute_ids)))]
@@ -567,11 +437,6 @@ def print_overall_summary(
         logger.info(
             '+' + ('-' * 16) + '+'
             + (('-' * 8) + '+') * len(compute_node_names))
-
-    _print_result_of_localagent(compute_ids, agent_results)
-    logger.info(
-        '+' + ('-' * 16) + '+'
-        + (('-' * 8) + '+') * len(compute_node_names))
     logger.info('=' * 70)
 
 
@@ -999,76 +864,22 @@ def main(bt_logger=None):
                         error_plugins, out_plugins[node_id])
 
     mcelog_delete()
-
-    agent_results = []
-    _print_label('Testing LocalAgent on compute nodes')
-    for compute_node in computes:
-        agent_server_running = conf.is_localagent_server_running(compute_node)
-        agent_infofetch_running = (
-            conf.is_localagent_infofetch_running(compute_node) and
-            conf.is_redis_running(compute_node))
-
-        if agent_server_running:
-            test_name = 'barotest'
-            tmpfile = '/tmp/' + test_name + '.conf'
-
-            agent_config = conf.get_localagent_config(compute_node)
-            listen_ip = compute_node.get_ip()
-            listen_port = agent_config.get('server').get('listen_port')
-            amqp_host = agent_config.get('server').get('amqp_host')
-            amqp_port = agent_config.get('server').get('amqp_port')
-            amqp_user = agent_config.get('server').get('amqp_user')
-            amqp_passwd = agent_config.get('server').get('amqp_password')
-            rest_client = RestLocalAgentClient(
-                              listen_ip, listen_port, '', '')
-            pub_client = PubLocalAgentClient(
-                             amqp_host, amqp_port, amqp_user,
-                             amqp_passwd)
-
-            all_res = True
-            for client in [rest_client, pub_client]:
-                tests.test_localagent_server_set_collectd(
-                    compute_node, tmpfile, logger, client)
-                sleep_time = 1
-                logger.info(
-                    'Sleeping for {} seconds'.format(sleep_time)
-                    + ' before localagent server test...')
-                res = conf.check_localagent_dummy_included(
-                          compute_node, test_name)
-                all_res = all_res and res
-
-            _process_localagent_result(
-                compute_node.get_id(), 'Server',
-                all_res, agent_results, compute_node.get_name())
-
-        if agent_infofetch_running:
-            test_name = 'barotest'
-            resources = conf.create_testvm(compute_node, test_name)
-            sleep_time = 5
-            logger.info(
-                'Sleeping for {} seconds'.format(sleep_time)
-                + ' before localagent infofetch test...')
-            time.sleep(sleep_time)
-            res = conf.test_localagent_infofetch_get_data(
-                      compute_node, test_name)
-            conf.delete_testvm(resources)
-
-            _process_localagent_result(
-                compute_node.get_id(), 'InfoFetch',
-                res, agent_results, compute_node.get_name())
-
     print_overall_summary(
-        compute_ids, plugin_labels, aodh_plugin_labels, results, out_plugins,
-        agent_results)
+        compute_ids, plugin_labels, aodh_plugin_labels, results, out_plugins)
 
-    for res in results + agent_results:
+    res_overall = 0
+    for res in results:
         if not res[3]:
             logger.error('Some tests have failed or have not been executed')
             logger.error('Overall Result is Fail')
-            return 1
+            res_overall = 1
         else:
             pass
-    return 0
+
+    _print_label('Testing LocalAgent on compute nodes')
+    res_agent = local_agent.local_agent_main(logger, conf, computes)
+
+    return 0 if res_overall == 0 and res_agent == 0 else 1
 
 
 if __name__ == '__main__':
